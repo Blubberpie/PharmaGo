54e053acd1eef4ba373ebcf6b49d7bdd34ac6f66
pao741 <anuvt741@gmail.com>
2021-03-03 20:04:35 +0700

    add router display to app.vue/ create basic interface in main page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+// import About from '../views/About.vue';
+
+const About = () => import('../views/About.vue');
 
 // const Login = () => import('@/components/Login.vue');
 
@@ -26,7 +29,9 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: About,
   },
 ];
 
@@ -50,6 +55,6 @@ const router = new VueRouter({
 
 // router.beforeEach(beforeRouteEnter);
 
-// Vue.$router = router;
+Vue.$router = router;
 
 export default router;
