506b38f6426d9a874ccc6db34f53b01d33e35342
Boss Udomsuktham <bossiccs@Bosss-MacBook-Pro.local>
2021-03-03 16:12:58 +0700

    added register 2

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,49 +1,46 @@
 <template>
      <v-app class='background'>
-        <v-container class='mt-15 '>
+      <v-container class='mt-15 '>
         <v-layout row align-center justify-center class='mt-15 '>
-        <v-flex xs12 md6 sm6 class=''>
-                <!-- <v-form > -->
-                <v-text-field class='ml-5 mr-5' outlined v-model='Username'
-                  label="Username">
-                </v-text-field>
-            <v-text-field class='ml-5 mr-5' v-model='Email' outlined :rules="emailRules"
+          <v-flex xs12 md6 sm6 class=''>
+            <v-text-field class='ml-5 mr-5' outlined v-model='username'
+              label="Username">
+            </v-text-field>
+            <v-text-field class='ml-5 mr-5' v-model='email' outlined :rules="emailRules"
               label="Email">
             </v-text-field>
-            <v-text-field class='ml-5 mr-5' v-model='Password' outlined
+            <v-text-field class='ml-5 mr-5' v-model='password' outlined
               label="Password"
-             type="password">
+              type="password">
             </v-text-field>
             <v-select class='ml-5 mr-5'
             :items="types"
             label="Type"
-            v-model="Role"
+            v-model="role"
             outlined
             ></v-select>
-                <!-- <v-card-actions class='justify-center'> -->
-                <v-btn depressed block primary @click="Register"
-                class='teal lighten-3 white--text'>Register
-                </v-btn>
-                <!-- </v-card-actions> -->
-                <!-- </v-form> -->
-            <!-- </v-card> -->
-        </v-flex>
-
+            <v-btn depressed block primary @click="Register"
+            class='teal lighten-3 white--text'>Register
+            </v-btn>
+          </v-flex>
         </v-layout>
-        </v-container>
+      </v-container>
     </v-app>
 </template>
 <script>
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
+
+const database = firebase.database();
 
 export default {
   data() {
     return {
-      Username: '',
-      Email: '',
-      Password: '',
-      Role: '',
+      username: '',
+      email: '',
+      password: '',
+      role: '',
       emailRules: [
         (v) => /[a-zA-z0-9._]+@[a-zA-Z]+\.[a-zA-z]{2,4}/.test(v) || 'Invalid email',
       ],
@@ -53,15 +50,15 @@ export default {
   methods: {
     async Register() {
       try {
-        console.log(this.Email);
-        console.log(this.Password);
-        firebase.auth().createUserWithEmailAndPassword(this.Email, this.Password).then(
-          (cred) => { console.log(cred); },
+        firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
+          (cred) => {
+            database.ref(`/user/${cred.user.uid}`).push({
+              username: this.username,
+              role: this.role,
+            });
+          },
         );
-        console.log('success');
-        // this.$router.replace({ name: 'Login' });
       } catch (e) {
-        console.log('failed');
         console.log(e);
       }
     },
