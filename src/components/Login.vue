b9f8f7337947345e7314c84aeb0498777706ca76
pratibha-rin <59640782+pratibha-rin@users.noreply.github.com>
2021-03-03 20:55:58 +0700

    Add files via upload

---
+++ src/components/Login.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <h2>Login</h2>
+        <form @submit.prevent="handleSubmit">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text"
+                v-model="username"
+                name="username"
+                class="form-control" :class="{ 'is-invalid': submitted && !username }" />
+                <div v-show="submitted && !username"
+                class="invalid-feedback">Username is required here.</div>
+            </div>
+            <div class="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="password"
+                v-model="password"
+                name="password"
+                class="form-control" :class="{ 'is-invalid': submitted && !password }" />
+                <div v-show="submitted && !password"
+                class="invalid-feedback">Password is required here.</div>
+            </div>
+            <div class="form-group">
+                <button class="btn btn-primary" :disabled="status.loggingIn">Login</button>
+                <img v-show="status.loggingIn"
+                src="data:image/gif;base64,R0lGODlhEAA
+                QAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05
+                FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAA
+                QAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJ
+                CgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihk
+                MZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9H
+                MaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxR
+                IdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMg
+                QwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQO
+                RfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQ
+                AAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAA
+                AALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnl
+                sRkAAAOwAAAAAAAAAAAA=="/>
+                <router-link to="/register" class="btn btn-link">Register</router-link>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      submitted: false,
+    };
+  },
+  computed: {
+    ...mapState('account', ['status']),
+  },
+  created() {
+    this.logout();
+  },
+  methods: {
+    ...mapActions('account', ['login', 'logout']),
+    handleSubmit() {
+      this.submitted = true;
+      const { username, password } = this;
+      if (username && password) {
+        this.login({ username, password });
+      }
+    },
+  },
+};
+</script>
