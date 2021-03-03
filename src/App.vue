d15a99f86c912cda79d3d488ebac9405d1fb5eaa
Zwel Pai <lozberipie@gmail.com>
2021-03-03 16:52:14 +0700

    .gitattributes for vue files to have LF

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <template>
   <v-app>
     <v-main>
-      <MapTest/>
+      <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import MapTest from './components/MapTest.vue';
-
 export default {
   name: 'App',
-
-  components: {
-    MapTest,
-  },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
