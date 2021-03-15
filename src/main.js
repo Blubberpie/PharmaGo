3d41ee1a7644fad5ff7d85e9795ac408befb8187
Zwel Pai <lozberipie@gmail.com>
2021-03-15 21:27:22 +0700

    Use vue-browser-geolocation

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import Vue from 'vue';
+import VueGeolocation from 'vue-browser-geolocation';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
+Vue.use(VueGeolocation);
 
 let isInitialized = false;
 const database = firebase.database();
